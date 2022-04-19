f5cf40cd384535a0bc28eee98d9ee0be38b52c62
Kyoungwon-Min <76611903+mkwkw@users.noreply.github.com>
2022-04-19 20:42:06 +0900

    Revert "Kyoungwon"

--- src/main/java/com/albatross/bareungeulssi/web/HomeController.java
+++ src/main/java/com/albatross/bareungeulssi/web/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    //@GetMapping("/")
+    @GetMapping("/")
     public String home() {
         return "home";
     }
